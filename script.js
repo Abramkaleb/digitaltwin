c1de9c6e186d803497abead0f7ddd4f55840d540
Abramkaleb <137379504+Abramkaleb@users.noreply.github.com>
2023-06-22 14:25:57 +0700

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Ambil elemen-elemen dengan ID yang sesuai
-  var temperatureElement = document.getElementById('temperature');
-  var rpmElement = document.getElementById('rpm');
-  var fuelElement = document.getElementById('fuel');
-  
-  // Fungsi untuk memperbarui nilai suhu
-  function updateTemperature(value) {
-    temperatureElement.textContent = value + ' °C';
-  }
-  
-  // Fungsi untuk memperbarui nilai RPM
-  function updateRPM(value) {
-    rpmElement.textContent = value + ' RPM';
-  }
-  
-  // Fungsi untuk memperbarui nilai Fuel
-  function updateFuel(value) {
-    fuelElement.textContent = value + '%';
-  }
-  
-  // Menghubungkan ke Arduino atau sumber data lainnya
-  // Gunakan teknik komunikasi seperti AJAX atau WebSocket
-  // untuk memperbarui nilai suhu, RPM, dan Fuel secara periodik
-  
-  // Contoh penggunaan data statis
-  var temperature = 25;
-  var rpm = 1500;
-  var fuel = 70;
-  
-  updateTemperature(temperature);
-  updateRPM(rpm);
-  updateFuel(fuel);
-});
+// Mendapatkan referensi elemen-elemen HTML
+const suhuValue = document.getElementById('suhu-value');
+const rpmValue = document.getElementById('rpm-value');
+const fuelValue = document.getElementById('fuel-value');
+
+// Fungsi untuk memperbarui nilai sensor
+function updateSensorValues(suhu, rpm, fuel) {
+  suhuValue.textContent = suhu + '°C';
+  rpmValue.textContent = rpm + ' RPM';
+  fuelValue.textContent = fuel + '%';
+}
+
+// Contoh pembaruan data sensor (dapat diganti dengan data aktual dari Arduino)
+const suhu = 28;
+const rpm = 2500;
+const fuel = 75;
+
+// Memanggil fungsi updateSensorValues dengan data aktual
+updateSensorValues(suhu, rpm, fuel);
