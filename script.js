980834a72825d39dc20cd6ec658077c22dba6896
Abramkaleb <137379504+Abramkaleb@users.noreply.github.com>
2023-06-22 14:19:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Ambil elemen-elemen dengan ID yang sesuai
   var temperatureElement = document.getElementById('temperature');
   var rpmElement = document.getElementById('rpm');
+  var fuelElement = document.getElementById('fuel');
   
   // Fungsi untuk memperbarui nilai suhu
   function updateTemperature(value) {
@@ -13,14 +14,21 @@ document.addEventListener('DOMContentLoaded', function() {
     rpmElement.textContent = value + ' RPM';
   }
   
+  // Fungsi untuk memperbarui nilai Fuel
+  function updateFuel(value) {
+    fuelElement.textContent = value + '%';
+  }
+  
   // Menghubungkan ke Arduino atau sumber data lainnya
   // Gunakan teknik komunikasi seperti AJAX atau WebSocket
-  // untuk memperbarui nilai suhu dan RPM secara periodik
+  // untuk memperbarui nilai suhu, RPM, dan Fuel secara periodik
   
   // Contoh penggunaan data statis
   var temperature = 25;
   var rpm = 1500;
+  var fuel = 70;
   
   updateTemperature(temperature);
   updateRPM(rpm);
+  updateFuel(fuel);
 });
